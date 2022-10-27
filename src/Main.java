baf1425af77ff67dcba4ef1fbd6974b9ab70dbbb
TaisiaKol <tasia.pcma.le4@mail.ru>
2022-10-27 15:56:19 +0300

    commit all

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int money = 1000;
+        int refill = 2000;
+        int fullMoney = (money + refill);
+        System.out.println(fullMoney);
+        int bonus;
+        if (refill > 1000) {
+            bonus = (refill / 100);
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+    }
+}
